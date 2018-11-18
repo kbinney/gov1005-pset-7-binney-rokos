385547be4270e8543affb1f4bf52c645b8afebba
Katherine Binney <kbinney@college.harvard.edu>
2018-11-18 11:49:12 -0500

    Add district demographic data

--- districtInfo.R
+++ districtInfo.R
@@ -22,15 +22,46 @@ education_factor <- elections_data %>%
   group_by(num_interviews, district) %>% 
   count(education) %>% 
   mutate(percent = 100 * n / num_interviews) %>% 
-  ungroup() %>% 
+  ungroup() %>%
+  filter(!is.na(education)) %>% 
   select(district, percent, education) %>% 
   spread(education, percent)
-  
 
-mutate(race_edu = parse_factor(race_edu, levels = NULL, include_na = FALSE, 
+write_rds(education_factor, path = "poll_diff/education_data")
+  
+race_edu_factor <- elections_data %>% 
+  group_by(district)  %>% 
+  mutate(race_edu = parse_factor(race_edu, levels = NULL, include_na = FALSE, 
                                na = c("", "na", "[DO NOT READ] Don't know/Refused")),
-       "Race and Education" = recode_factor(race_edu,
+         race_edu = recode_factor(race_edu,
                                             "Nonwhite" = "Nonwhite",
                                             "White, 4-Year College Grads" = "White, college grad",
                                             "White, No 4-Year College Degree" = "White, not college grad", 
-                                            .ordered = TRUE)) %>% 
+                                            .ordered = TRUE),
+       num_interviews = n()) %>% 
+  group_by(num_interviews, district) %>% 
+  count(race_edu) %>% 
+  mutate(percent = 100 * n / num_interviews) %>% 
+  ungroup() %>%
+  filter(!is.na(race_edu)) %>% 
+  select(district, percent, race_edu) %>% 
+  spread(race_edu, percent)
+
+write_rds(race_edu_factor, path = "poll_diff/race_edu")
+
+race_factor <- elections_data %>% 
+  group_by(district)  %>% 
+  mutate(race_eth = parse_factor(race_eth, levels = NULL, include_na = FALSE, 
+                                 na = c("", "na", "[DO NOT READ] Don't know/Refused")),
+         num_interviews = n()) %>% 
+  group_by(num_interviews, district) %>% 
+  count(race_eth) %>% 
+  mutate(percent = 100 * n / num_interviews) %>% 
+  ungroup() %>%
+  filter(!is.na(race_eth)) %>% 
+  select(district, percent, race_eth) %>% 
+  spread(race_eth, percent)
+
+write_rds(race_factor, path = "poll_diff/race_eth")
+
+
