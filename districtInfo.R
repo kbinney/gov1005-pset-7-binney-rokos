dcdc0794f91a859e981f9601c387744f38163dd3
Katherine Binney <kbinney@college.harvard.edu>
2018-11-19 14:16:35 -0500

    Add comments to demographic compilation

--- districtInfo.R
+++ districtInfo.R
@@ -1,14 +1,18 @@
 library(tidyverse)
 library(readr)
 
-# This file is used to take all interview level data and find out interesting things about each district's
-# demographics
+# This file is used to take all interview level data and find out interesting
+# things about each district's demographics
+
+# First, we read in preformatted polling data. This data has already been
+# compiled and joined with actual election data results.
 elections_data <- read_rds("poll_data")
 
-# 
+# We want to compare education to polling data. To do this, we first set the
+# education levels and set up our NA result. We then figure out the percentage
+# of each education level in each district. Finally, we remove the NA results.
 education_factor <- elections_data %>% 
   group_by(district)  %>% 
-  # Let's nicely refactor eduction
   mutate(education = parse_factor(educ4, ordered = TRUE, include_na = FALSE,
                                   na = c("", "NA", "[DO NOT READ] Don't know/Refused"),
                                   levels = c("High School Grad. or Less", 
@@ -24,6 +28,7 @@ education_factor <- elections_data %>%
   # Rename this column so all tables have demographic column 
   mutate(demographic = education)
 
+# We save this data to use in our shiny app as needed
 write_rds(education_factor, path = "poll_diff/education_data")
   
 race_edu_factor <- elections_data %>% 
