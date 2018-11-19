7a524ea1d5468fdd70caa6b06d4175ea763cbae8
Katherine Binney <kbinney@college.harvard.edu>
2018-11-19 10:57:39 -0500

    Add code to get polling dem advantage

--- app_background.R
+++ app_background.R
@@ -30,18 +30,52 @@ all_polls <- map_dfr(file_names, read_csv, .id = "source")
 # Cleaning the source names so that they are easier to work with
 all_polls$source <- str_remove(all_polls$source, "2018-live-poll-results-master/data/elections-poll-")
 all_polls$source <- str_remove(all_polls$source, ".csv")
-all_polls <- separate(all_polls, source, into = c("district", "wave"), sep = "-")
-all_polls <- separate(all_polls, district, into = c("state", "district_race"), sep = 2)
+all_polls <- all_polls %>% 
+  separate(source, into = c("district", "wave"), sep = "-") %>% 
+  separate(district, into = c("state", "district_race"), sep = 2) %>% 
+  mutate(district = parse_number(district_race, na = c("", "NA", "sen", "gov")),
+         district_race = case_when(district_race == "gov" ~ "gov",
+                                   district_race == "sen" ~ "sen",
+                                   TRUE ~ as.character(district))) 
+
+# Below, we calculate the dem advantage and votes in dem, rep and other for each
+# polled district, based on info from final wave of polling
+poll_dem_advantage <- all_polls %>% 
+  # We need to figure out which poll is latest for each election, and
+  # then calculate the dem advantage (as that is reported in actual results) in each race
+  mutate(wave = parse_number(wave)) %>% 
+  group_by(state, district_race) %>% 
+  # this allows us to find max poll in a race
+  mutate(max_wave = max(wave, na.rm = TRUE)) %>%
+  # filter for columns in final poll for race
+  filter(wave == max_wave) %>% 
+  mutate(group_wt = sum(final_weight)) %>% 
+  # To calculate responses, we need to group by poll
+  # and response. To keep our group_wt number around
+  # we also need to include that variable in our grouping
+  group_by(state, district_race, group_wt, response) %>% 
+  tally(wt = final_weight) %>% 
+  # Spreading our response into columns makes it easier to 
+  # calculate the republican advantage
+  spread(response, n) %>% 
+  summarize(dem_poll = sum(Dem),
+            rep_poll = sum(Rep),
+            other_poll = group_wt - dem_poll - rep_poll,
+            dem_advantage = 100 * (dem_poll - rep_poll) / group_wt) %>% 
+  # I want to join the tables to have state, race_type, district as 
+  # [State]-[district num, sen, gov], dem, rep, other, rep_advantage_poll, rep_advantage
+  ungroup() %>% 
+  select(everything(), -group_wt)
 
 
 # CREATING A DATA FRAME THAT INCLUDES THE RESULTS OF EACH RACE AS WELL (STILL AT POLL LEVEL)
 # Downloaded results data from link provided in piazza and moved it to this project as "2018_House_Results.csv"
 # Now reading in the results data
-house_results <- read_csv("2018_House_Results.csv", col_names = TRUE)
+house_results_read <- read_csv("2018_House_Results.csv", col_names = TRUE) 
 
 # Need to join House results data with polling data. 
 # Cleaning the House results data so that it is ideal for joining
-house_results <- house_results %>% 
+house_results <- house_results_read %>% 
   mutate(`Dem %` = str_replace(`Dem %`, "%", ""),
          `Dem %` = parse_double(`Dem %`),
          `GOP %` = str_replace(`GOP %`, "%", ""),
@@ -53,7 +87,7 @@ house_results <- house_results %>%
          `2016 Clinton Margin` = str_replace(`2016 Clinton Margin`, "%", ""),
          `2016 Clinton Margin` = parse_double(`2016 Clinton Margin`),
          `Swing vs. 2016 Prez` = str_replace(`Swing vs. 2016 Prez`, "%", ""),
-         `Swing vs. 2016 Prez` = parse_double(`Swing vs. 2016 Prez`),
+         `Swing vs. 2016 Prez` = parse_double(`Swing vs. 2016 Prez`, na = c("", "NA", "N/A")),
          `Raw Votes vs. 2016` = str_replace(`Raw Votes vs. 2016`, "%", ""),
          `Raw Votes vs. 2016` = parse_double(`Raw Votes vs. 2016`)) %>% 
   transmute(state_name = State, 
@@ -81,10 +115,8 @@ house_results <- house_results %>%
 
 # Binding the two data sets so that we have each poll response and the actual outcome for the district of that response
 results_and_polls <- all_polls %>% 
-  mutate(district_race = case_when(district_race == "gov" ~ "gov",
-                                   district_race == "sen" ~ "sen",
-                                   TRUE ~ as.character(parse_number(district_race)))) %>% 
   left_join(house_results, by = c("state", "district_race")) %>% 
+  left_join(poll_dem_advantage) %>% 
   mutate(district = str_c(state, "-", district_race)) %>% 
   select(-36:-86)   # dropping unnecessary survey questions
 
@@ -119,6 +151,7 @@ results_and_polls <- results_and_polls %>%
          not_called = case_when(district %in% c("ca-39", "ut-4", 
                                                 "fl-gov", "fl-sen") ~ 0.5,
                                 TRUE ~ 1))
+
 
 # We store this data for use in figuring out interesting things about each district
 write_rds(results_and_polls, path = "poll_data")
